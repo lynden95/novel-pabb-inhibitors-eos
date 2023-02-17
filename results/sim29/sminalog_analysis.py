877831bf130fde7cbd1503ef2e5e7d1595f24517
LYNDEN ROOMS <lynden.rooms@bristol.ac.uk>
2023-02-17 09:04:04 +0100

    All files from the week

---
+++ results/sim29/sminalog_analysis.py
@@ -0,0 +1,35 @@
+import os
+import itertools
+import collections
+import pprint
+import sys
+import os.path
+import glob
+import re
+
+mypath = os.path.abspath(os.getcwd())                                           # get path of current dir
+
+print("Directory path detected : ", mypath)
+
+logfile = glob.glob('*log*.txt')                                #getting the log filename
+print(logfile)
+file_path = os.path.join(mypath, logfile[0])
+
+
+print("\nFile path detected : ", file_path)
+
+with open(logfile[0], "r") as f:
+    i = 0;
+    for line in f:
+        if '-+' in line:
+            nextline = next(f)
+            i = i + 1
+
+            nextlinearray  = nextline.split()                       #splitting the first row in different values
+            bind_aff = nextlinearray[1]                             #getting the binding affinity of first pose
+
+            with open("output.txt", "a") as myfile:
+                 print(i, " : ", bind_aff, end='\n', file=myfile)
+
+
+    print("Done! The result is provided in the output.txt file.")
