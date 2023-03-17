7c44555252b0c66af8ed5d80a5f7a13df5b88c57
LYNDEN ROOMS <lynden.rooms@bristol.ac.uk>
2023-03-17 13:02:09 +0100

    ready for use!

--- src/scorer.py
+++ src/scorer.py
@@ -1,52 +1,63 @@
-#import all libraries 
+# import all libraries
 import os
+import sys
 import pandas as pd
 import numpy as np
 from rdkit import Chem
 from rdkit.Chem import AllChem
-from rdkit.Chem import rdDistGeom
 from rdkit.Chem.rdmolfiles import MolToPDBFile
-from rdkit.Chem import Descriptors
-from rdkit.ML.Descriptors import MoleculeDescriptors
 from rdkit.Chem import PandasTools
-from rdkit.Chem.Draw import IPythonConsole
-from rdkit.Chem import Draw
+from rdkit.Chem import Descriptors
 from standardiser import standardise
+import subprocess
+import shutil
 
+# define pathnames
+root = os.path.dirname(os.path.abspath(__file__))
 
+PACKAGEDATAPATH = os.path.join(root, "..", "data")
+DATAPATH = os.path.abspath(os.path.join(root, "../data/"))
+RESULTSPATH = os.path.abspath(os.path.join(root, "../results/"))
+SOURCEPATH = os.path.abspath(os.path.join(root, "../src"))
 
-#define pathnames
-DATAPATH = "../data/"
-RESULTSPATH = "../results/"
-SOURCEPATH = "../src"
+# define standard arguments/parameters
 
-#define standard arguments/parameters
+# smiles prep - standardise and filter
+smiles_csv = sys.argv[1]
+output_folder = sys.argv[2]
 
-#smiles prep - standardise and filter
-smiles_csv = os.path.join(DATAPATH, "smiles", "smiles.csv")
-filtered_std_smiles = os.path.join(DATAPATH, "smiles", "filtered_std_smiles.csv") 
+if os.path.exists(output_folder):
+    print(output_folder, " already exists")
+    sys.exit*()
 
-#ligand prep
+os.mkdir(output_folder)
+
+DATAPATH = os.path.join(output_folder, "data")
+RESULTSPATH = os.path.join(output_folder, "results")
+os.mkdir(DATAPATH)
+os.mkdir(RESULTSPATH)
+os.mkdir(os.path.join(DATAPATH, "smiles"))
+os.mkdir(os.path.join(RESULTSPATH, "outputs"))
+
+affinities_id_smiles = os.path.join(RESULTSPATH, "output.csv")
+
+filtered_std_smiles = os.path.join(DATAPATH, "smiles", "filtered_std_smiles.csv")
+
+# ligand prep
 sdf_folder = os.path.join(DATAPATH, "smiles")
 pH = 7.4
 
-#docking
-receptor = os.path.join(DATAPATH, "protein", "pabb_model1.pdbqt") 
-ligands = os.path.join(DATAPATH, "smiles", "merged.sdf")  
-logfile = os.path.join(RESULTSPATH, "outputs", "log.txt") 
-docking_output = os.path.join(RESULTSPATH, "outputs", "outputs.sdf") 
-smina = os.path.join(SOURCEPATH, "smina.static") 
-
-#analysis
-affinities_id_smiles = os.path.join(RESULTSPATH, "outputs", "affinity_id_smiles.csv")
+# docking
+receptor = os.path.join(PACKAGEDATAPATH, "protein", "pabb_model1.pdbqt")
+ligands = os.path.join(DATAPATH, "smiles", "merged.sdf")
+logfile = os.path.join(RESULTSPATH, "outputs", "log.txt")
+#docking_output = os.path.join(RESULTSPATH, "outputs", "outputs.sdf")
+smina = os.path.join(SOURCEPATH, "smina.static")
 
 
-
-#define functions
-
+# define functions
 def prep_smiles(smiles_csv):
-
-    df=pd.read_csv(smiles_csv) 
+    df = pd.read_csv(smiles_csv)
 
     mols = [Chem.MolFromSmiles(smi) for smi in df["SMILES"].tolist()]
 
@@ -66,84 +77,82 @@ def prep_smiles(smiles_csv):
 
     for std_mol in std_mols:
         if std_mol is not None:
-            try: 
+            try:
                 std_smi = Chem.MolToSmiles(std_mol)
             except:
-                std_smi=np.nan
+                std_smi = np.nan
         else:
             std_smi = np.nan
         std_smiles += [std_smi]
 
     df["ST_SMILES"] = std_smiles
 
-    df=df[df["ST_SMILES"].notna()]
+    df = df[df["ST_SMILES"].notna()]
 
-    df.drop(columns = ["SMILES"], inplace=True)
+    df.drop(columns=["SMILES"], inplace=True)
 
     mols = [Chem.MolFromSmiles(smi) for smi in df["ST_SMILES"].tolist()]
 
     mol_wts = []
 
     for mol in mols:
-        mol_wt = Chem.Descriptors.MolWt(mol)
+        mol_wt = Descriptors.MolWt(mol)
         mol_wts += [mol_wt]
 
     df["mol_wt"] = mol_wts
 
-    df_filtered = df[df['mol_wt'] >= 300]
-    df_filtered = df_filtered[df_filtered['mol_wt'] <= 800]
-    df_filtered.drop(columns = ["mol_wt"], inplace=True)
+    df_filtered = df[df["mol_wt"] >= 300]
+    df_filtered = df_filtered[df_filtered["mol_wt"] <= 800]
+    df_filtered.drop(columns=["mol_wt"], inplace=True)
     df_filtered.to_csv(filtered_std_smiles, index=False)
 
 
+def prepare_ligands_sdf(
+    filtered_std_smiles, pH, header_len=1, output_dir=sdf_folder, delim=","
+) -> list:
+    out_sdfs = []
 
-def prepare_ligands_sdf(filtered_std_smiles, pH, header_len=1, output_dir = sdf_folder, delim=',') -> list:
-
-    out_sdfs = [] 
-    
     print(filtered_std_smiles)
-    
-    with open(filtered_std_smiles, 'r') as csv: 
-            
+
+    with open(filtered_std_smiles, "r") as csv:
         for entry in csv.readlines()[header_len:]:
-            
-            ID, ST_SMILES = entry.split(delim)[:2]            
-           
+            ID, ST_SMILES = entry.split(delim)[:2]
+
             # Convert smiles str to 3D coordinates
             mol = Chem.MolFromSmiles(ST_SMILES)
             mol = Chem.AddHs(mol)
-            ps = AllChem.ETKDGv2()
+            ps = AllChem.ETKDGv3()
             ps.useRandomCoords = True
             result = AllChem.EmbedMolecule(mol, ps)
-                        
+
             if type(result) is int:
                 if result == -1:
                     continue
-           
+
             # Ouput coords to pdb
             pdb_name = f"{output_dir}/{ID}.pdb"
             MolToPDBFile(mol, pdb_name)
-       
+
             # Protonate according to pH, convert to .sdf
             sdf_name = f"{output_dir}/{ID}.sdf"
-            ! obabel {pdb_name} -pH {pH} -O {sdf_name} 
-          
-            os.remove(pdb_name) # removes the .pdb files after obabel protonates and converts to .sdf
-                       
-            out_sdfs.append(sdf_name)
+            cmd = "obabel {0} -pH {1} -O {2}".format(pdb_name, pH, sdf_name)
+            print(cmd)
+            subprocess.Popen(cmd, shell=True).wait()
 
-            os.remove(filtered_std_smiles)
+            os.remove(
+                pdb_name
+            )  # removes the .pdb files after obabel protonates and converts to .sdf
+
+            out_sdfs.append(sdf_name)
 
     return out_sdfs
 
 
-
 def merge_sdfs(out_sdfs, merged_sdf):
-
     mols = []
     for s in out_sdfs:
         suppl = Chem.SDMolSupplier(s)
-        for  mol in suppl:
+        for mol in suppl:
             if mol is None:
                 continue
         else:
@@ -158,21 +167,47 @@ def merge_sdfs(out_sdfs, merged_sdf):
                 w.write(mol)
 
 
+def prepare_and_merge_ligands(
+    filtered_std_smiles, pH, header_len=1, output_dir="", delim=","
+):
+    ligands = prepare_ligands_sdf(
+        filtered_std_smiles, pH, header_len, output_dir, delim
+    )
+    merge_sdfs(ligands, output_dir + "/merged.sdf")
 
-def prepare_and_merge_ligands(filtered_std_smiles,  pH, header_len=1, output_dir='', delim=','):
-    ligands = prepare_ligands_sdf(filtered_std_smiles,  pH, header_len, output_dir, delim)
-    merge_sdfs(ligands, output_dir+"/merged.sdf") 
-
-
-
+docking_output = os.path.join(RESULTSPATH, "outputs", "docking-poses.sdf")
 def docking_cmd():
-    smina  + " -r " + receptor + " -l " + ligands + " -o " + docking_output + " --log " + logfile + " --seed 42 " + " --center_x 74 " + " --center_y 44 " + " --center_z 57 " + " --size_x 22 " + " --size_y 22 " + " --size_z 24 " + " --exhaustiveness 8 " + " --num_modes 1 " + " --addH off " + " --scoring vinardo "
+    cmd = (
+        smina
+        + " -r "
+        + receptor
+        + " -l "
+        + ligands
+        + " -o "
+        + docking_output
+        + " --log "
+        + logfile
+        + " --seed 42 "
+        + " --center_x 74 "
+        + " --center_y 44 "
+        + " --center_z 57 "
+        + " --size_x 22 "
+        + " --size_y 22 "
+        + " --size_z 24 "
+        + " --exhaustiveness 8 "
+        + " --num_modes 1 "
+        + " --addH off "
+        + " --scoring vinardo "
+    )
+    print(cmd)
+    subprocess.Popen(cmd, shell=True).wait()
 
 
 def affinities_to_smiles(docking_output):
-
-    df = PandasTools.LoadSDF(docking_output, embedProps=True, molColName=None, smilesName='SMILES')
-    df['ID'] = df['ID'].map(lambda x: x.lstrip('../data/smiles/').rstrip('.pdb'))
+    df = PandasTools.LoadSDF(
+        docking_output, embedProps=True, molColName=None, smilesName="SMILES"
+    )
+    df["ID"] = df["ID"].map(lambda x: x.split("/smiles/")[1].rstrip(".pdb"))
     df.to_csv(affinities_id_smiles, index=False)
 
 
@@ -180,10 +215,13 @@ def run():
     prep_smiles(smiles_csv)
     prepare_and_merge_ligands(filtered_std_smiles, pH, output_dir=sdf_folder)
     os.remove(filtered_std_smiles)
-    os.system(docking_cmd)
+    docking_cmd()
     os.remove(ligands)
     affinities_to_smiles(docking_output)
+    shutil.rmtree(DATAPATH)
+    shutil.move(os.path.join(RESULTSPATH, "output.csv"), os.path.join(output_folder, "output.csv"))
+    shutil.move(os.path.join(RESULTSPATH, "outputs", "log.txt"), os.path.join(output_folder, "log.txt"))
+    shutil.move(os.path.join(RESULTSPATH, "outputs", "docking-poses.sdf"), os.path.join(output_folder, "docking-poses.sdf"))
+    shutil.rmtree(RESULTSPATH)
 
-
-    
-    
+run()
--- src/sminalog_analysis.py
+++ src/sminalog_analysis.py
@@ -7,11 +7,11 @@ import os.path
 import glob
 import re
 
-mypath = os.path.abspath(os.getcwd())                                           # get path of current dir
+mypath = os.path.abspath(os.getcwd())  # get path of current dir
 
 print("Directory path detected : ", mypath)
 
-logfile = glob.glob('*log*.txt')                                #getting the log filename
+logfile = glob.glob("*log*.txt")  # getting the log filename
 print(logfile)
 file_path = os.path.join(mypath, logfile[0])
 
@@ -19,17 +19,18 @@ file_path = os.path.join(mypath, logfile[0])
 print("\nFile path detected : ", file_path)
 
 with open(logfile[0], "r") as f:
-    i = 0;
+    i = 0
     for line in f:
-        if '-+' in line:
+        if "-+" in line:
             nextline = next(f)
             i = i + 1
 
-            nextlinearray  = nextline.split()                       #splitting the first row in different values
-            bind_aff = nextlinearray[1]                             #getting the binding affinity of first pose
+            nextlinearray = (
+                nextline.split()
+            )  # splitting the first row in different values
+            bind_aff = nextlinearray[1]  # getting the binding affinity of first pose
 
             with open("output.txt", "a") as myfile:
-                 print(i, " : ", bind_aff, end='\n', file=myfile)
-
+                print(i, " : ", bind_aff, end="\n", file=myfile)
 
     print("Done! The result is provided in the output.txt file.")
